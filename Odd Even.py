ed4b2b472a2065de4df0c5dfaf2c78bc0111e487
AMD <prerakdhumale025@gmail.com>
2026-05-17 17:27:39 +0530

    Odd Even python code

---
+++ Odd Even.py
@@ -0,0 +1,9 @@
+#Write function to identify if number is even or odd.
+def even_or_odd(num):
+    if num % 2 == 0:
+        print(num, "is Even")
+    else:
+        print(num, "is Odd")
+
+# Function call
+even_or_odd(7)
