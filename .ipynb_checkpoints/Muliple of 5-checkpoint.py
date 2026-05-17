63c666369c0eebd39ead222e4383ab78c49231c6
AMD <prerakdhumale025@gmail.com>
2026-05-17 17:46:04 +0530

    Checking Multiple of 5

---
+++ .ipynb_checkpoints/Muliple of 5-checkpoint.py
@@ -0,0 +1,7 @@
+#Check if given number is multiple of 5
+num = int(input("Enter a number: "))
+
+if num % 5 == 0:
+    print(num, "is a multiple of 5")
+else:
+    print(num, "is not a multiple of 5")
